b692a68299dd1d7a33a5fa623c5f03f3bdb15a6b
INABIK4RI <rubenedgar34@gmail.com>
2025-04-07 08:48:54 +0200

    Did Sort_Pixel

--- Day 2/Sort_Pixel/sketch2.js
+++ Day 2/Sort_Pixel/sketch2.js
@@ -13,20 +13,31 @@ function draw() {
     background(0);
     image(img, 100, 100, 750, 400);
     img.loadPixels();
+
+    let pixelsArray = [];
     for (let y = 0; y < img.height; y++) {
         for (let x = 0; x < img.width; x++) {
             let i = (x + y * img.width) * 4;
-            
             let r = img.pixels[i + 0];
             let g = img.pixels[i + 1];
             let b = img.pixels[i + 2];
             img.pixels[i + 3] = 255;
-
-            let sorted = [r, g, b].sort(function(a, b) {return a - b });
-            img.pixels[i + 0] = sorted[0];
-            img.pixels[i + 1] = sorted[1];
-            img.pixels[i + 2] = sorted[2];
+            pixelsArray.push([r, g, b]);
         }
     }
+
+    pixelsArray.sort((a, b) => hue(a) - hue(b));
+    let index = 0;
+    for (let y = 0; y < img.height; y++) {
+        for (let x = 0; x < img.width; x++) {
+            let i = (x + y * img.width) * 4;
+            img.pixels[i + 0] = pixelsArray[index][0];
+            img.pixels[i + 1] = pixelsArray[index][1];
+            img.pixels[i + 2] = pixelsArray[index][2];
+            index++;
+        }
+    }
+
+
     img.updatePixels();
 }
